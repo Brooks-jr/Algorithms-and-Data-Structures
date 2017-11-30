c3f1bc99e2bf57723719ec15465058923789e64d
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-11-29 23:09:41 -0600

    bubble and selection done

--- Sorts.js
+++ Sorts.js
@@ -1,8 +1,50 @@
+
+
+var arr = [88, 3, 5, 2, 23, 1, 9, 10, 7, 90];
+
 //Bubble
+function BubbleSort(arr) {
+    var temp;
+    for (let i = 0; i < arr.length; i++) {
+        for (let x = 0; x < arr.length; x++) {
+            if (arr[x] > arr[x + 1]) {
+                temp = arr[x];
+                arr[x] = arr[x + 1];
+                arr[x + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+BubbleSort(arr)
+// ===========================================================
 
 //Selection
+function SelectionSort(arr) {
+    var temp;
+    for (let i = 0; i < arr.length; i++) {
+        var min = i;
+
+        for (let x = i + 1; x < arr.length; x++) {
+            if (arr[x] < arr[min]) {
+                min = x;
+            }
+        }
+        if (min !== i) {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    return arr;
+}
+SelectionSort(arr)
+// ===========================================================
 
 //Insertion
+function InsertionSort(arr) {
+    
+}
 
 //Merge
 
