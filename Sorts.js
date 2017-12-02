4501e5689f4f37d6a49cb7d99aad98023542607d
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-12-02 15:58:06 -0600

    ...

--- Sorts.js
+++ Sorts.js
@@ -19,7 +19,9 @@ function BubbleSort(arr) {
 BubbleSort(arr);
 // ===========================================================
 
-//Selection
+//Selection | Complexity: the worst case O(n2) & best case O(n)
+
+/* arry */
 function SelectionSort(arr) {
     var temp;
     for (let i = 0; i < arr.length; i++) {
@@ -39,26 +41,48 @@ function SelectionSort(arr) {
     return arr;
 }
 SelectionSort(arr);
+
+/* string */
+function SelectionSort(str) {
+    var temp;
+    str = str.split('');
+    for (let i = 0; i < str.length; i++) {
+        var min = i;
+
+        for (let x = i + 1; x < str.length; x++) {
+            if (str[x] < str[min]) {
+                min = x;
+            }
+        }
+        if (min !== i) {
+            temp = str[i];
+            str[i] = str[min];
+            str[min] = temp;
+        }
+    }
+    return str.join('');
+}
+SelectionSort("d4c3b2a1");
 // ===========================================================
 
 //Insertion
-// function InsertionSort(arr) {
-//     for (let i = 1; i < arr.length; i++) {
-//         var x = i;
-//         while (x > 0 && arr[x - 1] > arr[i]) {
-//             arr[x] = arr[x - 1];
-//             x -= 1;
-//         }
-//         arr[x] = arr[i];
-//     }
-//     return arr;
-// }
-// InsertionSort(arr);
+function InsertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        var x = i;
+        while (x > 0 && arr[x - 1] > arr[i]) {
+            arr[x] = arr[x - 1];
+            x --;
+        }
+        arr[x] = arr[i];
+    }
+    return arr;
+}
+InsertionSort(arr);
 // ===========================================================
 
 //Merge
 function MergeSort(arr) {
-    
+
 }
 // ===========================================================
 
