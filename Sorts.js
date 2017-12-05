3e3ce54d823327b9fdf98a721bb71442fc09beb3
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-12-05 02:13:50 -0600

    sorts

--- Sorts.js
+++ Sorts.js
@@ -16,7 +16,7 @@ function BubbleSort(arr) {
     }
     return arr;
 }
-BubbleSort(arr);
+// BubbleSort(arr);
 // ===========================================================
 
 //Selection | Complexity: the worst case O(n2) & best case O(n)
@@ -71,7 +71,7 @@ function InsertionSort(arr) {
         var x = i;
         while (x > 0 && arr[x - 1] > arr[i]) {
             arr[x] = arr[x - 1];
-            x --;
+            x--;
         }
         arr[x] = arr[i];
     }
@@ -81,9 +81,44 @@ InsertionSort(arr);
 // ===========================================================
 
 //Merge
-function MergeSort(arr) {
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        // return once we hit an array with a single item
+        return arr
+    }
+    
 
+    const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
+    const left = arr.slice(0, middle) // items on the left side
+    const right = arr.slice(middle) // items on the right side
+
+    return merge(
+        mergeSort(left),
+        mergeSort(right)
+    )
+}
+
+// compare the arrays item by item and return the concatenated result
+function merge(left, right) {
+    let result = []
+    let indexLeft = 0
+    let indexRight = 0
+
+    while (indexLeft < left.length && indexRight < right.length) {
+        if (left[indexLeft] < right[indexRight]) {
+            result.push(left[indexLeft]);
+            indexLeft++;
+        } else {
+            result.push(right[indexRight])
+            indexRight++
+        }
+    }
+
+    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
 }
 // ===========================================================
 
 //Quick
+function quickSort(arr ) {
+    
+}
