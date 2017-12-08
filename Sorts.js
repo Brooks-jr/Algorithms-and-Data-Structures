fb2025c86919e06ad8fc6006c37f1ff1a23f0695
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-12-08 11:03:19 -0600

    CS

--- Sorts.js
+++ Sorts.js
@@ -86,8 +86,6 @@ function mergeSort(arr) {
         // return once we hit an array with a single item
         return arr
     }
-    
-
     const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
     const left = arr.slice(0, middle) // items on the left side
     const right = arr.slice(middle) // items on the right side
