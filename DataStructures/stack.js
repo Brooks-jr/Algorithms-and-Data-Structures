0cdb96dec3de3ba22b5c1176a8921767259800a1
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-12-12 18:02:56 -0600

    DataStructure Stacks

---
+++ DataStructures/stack.js
@@ -0,0 +1,21 @@
+var Stack = (function () {
+    function Stack() {
+        this.dataStore = [];
+        this.top = 0;
+    }
+    Stack.prototype.push = function (item) {
+        this.dataStore.push(item);
+        this.top = this.dataStore.length - 1;
+    }
+    
+    Stack.prototype.pop = function () {
+        this.dataStore.pop();
+        this.top = this.dataStore.length - 1; 
+    }
+    Stack.prototype.display = function () {
+        console.log(this.dataStore);
+        console.log("Top is now " + this.dataStore[this.top]);    
+    }
+    return Stack;
+})();
+module.exports = Stack;
